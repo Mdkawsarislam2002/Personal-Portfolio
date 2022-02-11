68ce6fa8a0b6b3b4ae5898f7338dcca43a0a2306
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-02-12 00:17:16 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        seondColor: "rgb(0,207,93)",
+        sideColor: "rgb(10, 184, 157)",
+      },
+    },
+  },
+  plugins: [],
+};
