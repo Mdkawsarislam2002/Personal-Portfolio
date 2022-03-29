f5e9abfd673369e8888f044183b52e8de73472eb
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-03-30 01:49:30 +0600

    Added hamburger menu.

--- public/script.js
+++ public/script.js
@@ -3,3 +3,10 @@ let preLoader = document.querySelector("#loader");
 window.addEventListener("load", () => {
   preLoader.style.display = "none";
 });
+
+let mobilMenuContent = document.getElementById("mobile-content");
+let mobileMenuBtn = document.getElementById("mobile-icon");
+
+mobileMenuBtn.addEventListener("click", () => {
+  mobilMenuContent.classList.toggle("toggle");
+});
