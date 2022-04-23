dfac08e69667e038b96aa285901be16965300bb5
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-04-24 00:54:31 +0600

    final commit

--- output/script.js
+++ output/script.js
@@ -1,17 +1,19 @@
-{
-  // =========================   Pre-Loader   ==================================== //
-  let preLoader = document.querySelector("#loader");
-  window.addEventListener("load", () => {
-    preLoader.style.display = "none";
-  });
+// =========================   Pre-Loader  start ==================================== //
+let preLoader = document.querySelector("#loader");
+window.addEventListener("load", () => {
+  preLoader.style.display = "none";
+});
+// =========================   Pre-Loader   end ====================================
+// =========================   Menu bar  start====================================
+let mobilMenuContent = document.getElementById("mobile-content");
+let mobileMenuBtn = document.getElementById("mobile-icon");
 
-  let mobilMenuContent = document.getElementById("mobile-content");
-  let mobileMenuBtn = document.getElementById("mobile-icon");
+mobileMenuBtn.addEventListener("click", () => {
+  mobilMenuContent.classList.toggle("toggle");
+});
 
-  mobileMenuBtn.addEventListener("click", () => {
-    mobilMenuContent.classList.toggle("toggle");
-  });
-}
+// =========================   Menu bar  End  ====================================
+
 const ResumeBtn = document.getElementById("ResumeBtn");
 ResumeBtn.addEventListener("click", (tag) => {
   tag.preventDefault();
